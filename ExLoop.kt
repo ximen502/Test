1df3684e0ec0b5516767c3403d0edcfac8ba266f
ximen502 <ximen502@126.com>
2021-11-27 16:06:06 +0800

    kotlin for循环遍历练习

---
+++ ExLoop.kt
@@ -0,0 +1,17 @@
+fun main() {
+    loopTraversal()
+}
+
+//循环遍历练习
+private fun loopTraversal() {
+    //var list = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    val list = listOf("赵一伤", "钱二败", "孙三毁", "李四摧", "周五输", "吴六破", "郑七灭", "王八衰")
+    for (item in list) {
+        println(item)
+    }
+
+    for (index in list.indices) {
+        //println("hero at $index is ${list.get(index)}")
+        println("hero at $index is ${list[index]}")
+    }
+}
