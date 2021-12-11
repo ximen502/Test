b188b6f89eea5f4cd4138f075d6be29609ba4328
ximen502 <ximen502@126.com>
2021-12-11 23:00:15 +0800

    kotlin Set的练习

---
+++ ExSet.kt
@@ -0,0 +1,31 @@
+fun ex() {
+    var set = setOf("Java", "Kotlin", "Go")
+    println(set)
+
+    var mutableSet = mutableSetOf("Java", "Kotlin", "Go")
+    println(mutableSet)//
+    println("setOf的返回对象的实际类型：${set.javaClass}")
+    println("mutableSet的返回对象的实际类型:${mutableSet.javaClass}")
+
+    var hashSet = hashSetOf("Java", "Kotlin", "Go")
+    println(hashSet) // 不保证元素的顺序
+
+    var linkedHashSet = linkedSetOf("Java", "Kotlin", "Go")
+    println(linkedHashSet) // 集合元素由小到大排序
+
+    var treeSet = sortedSetOf("Java", "Kotlin", "Go")
+    println(treeSet) // 集合元素由小到大排序
+}
+
+fun main() {
+    ex()
+}
+/* kotlin collection exercise: HashSet, LinkedHashSet, TreeSet */
+
+//[Java, Kotlin, Go]
+//[Java, Kotlin, Go]
+//setOf的返回对象的实际类型：class java.util.LinkedHashSet
+//mutableSet的返回对象的实际类型:class java.util.LinkedHashSet
+//[Go, Java, Kotlin]
+//[Java, Kotlin, Go]
+//[Go, Java, Kotlin]
