6ad349046f1d8d93fe8b022a211bd1c0da6dfb45
ximen502 <ximen502@126.com>
2021-12-12 20:07:35 +0800

    Set集合部分方法的练习2

--- ExSet2.kt
+++ ExSet2.kt
@@ -16,6 +16,42 @@ fun ex2() {
     // 返回删除前2个元素的集合
     val droppedList = set.drop(2)
     println(droppedList)
+
+    // 对Set集合元素进行过滤，要求包含"li"
+    val filteredList = set.filter { "li" in it }
+    println(filteredList)
+    // 返回集合中包含li的元素，找到就返回，找不到就返回null
+    val foundStr = set.find { "li" in it }
+    println(foundStr)
+    // 返回集合中包含gang的元素，找到就返回，找不到就返回null
+    val foundStr2 = set.find { "gang" in it }
+    println(foundStr2)
+    // 将set集合中所有的字符串拼接在一起
+    val foldedList = set.fold("", { acc, e -> acc + e })
+    println(foldedList)
+
+    // 查找某个元素出现的位置
+    println(set.indexOf("Go"))
+
+    val mappedList = set.map { "《疯狂" + it + "讲义" }
+    println(mappedList)
+    // max
+    println(set.maxOrNull())
+    // min
+    println(set.minOrNull())
+
+    val reversedList = set.reversed()
+    println(reversedList)
+
+    var bSet = setOf("Lua", "Erlang", "Kotlin")
+    //交集
+    println(set intersect bSet)
+    // 并集
+    println(set union bSet)
+    // 并集
+    println(set + bSet)
+    // set去除自身和bSet公共的元素
+    println(set - bSet)
 }
 
 fun main() {
