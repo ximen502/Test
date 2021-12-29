50ad137286b6ebc07c6d752d5bc48528e1b67c37
ximen502 <ximen502@126.com>
2021-12-29 23:56:52 +0800

    is运算符的练习2

--- ExIs.kt
+++ ExIs.kt
@@ -9,4 +9,11 @@ fun main(args: Array<String>) {
     println("whether string implement the Testable protocol:${hello is Testable}")
     val a: String = "Hello"
 //    println("is string a instance of Date:${a is Date}")
+
+    // is auto transform to target type
+    var b: Any = "zhaoyishang"
+    //编译时类型是Any，因此编译时没有length属性
+//    println(b.length)
+//is判断后类型自动转换为String
+    if (b is String) println(b.length)
 }
