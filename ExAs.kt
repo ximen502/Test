2ec07cb8658d64d038dbe772a356703659a61f73
ximen502 <ximen502@126.com>
2022-01-01 09:48:07 +0800

    as类型转换练习

---
+++ ExAs.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    val obj: Any = "ZhaoYishang"
+    val objStr = obj as String
+    println(objStr)
+    val objPri: Any = 5
+    val str: String = objPri as String
+    val fkit = "fkit.org"
+    val s: String = "Kotlin"
+    val num: Number = s as Number
+}
