1b714818f04129482699e05b999b5bea8cd91902
ximen502 <ximen502@126.com>
2022-01-15 10:57:56 +0800

    Java调用kotlin的扩展函数练习

---
+++ InvokeExtension.java
@@ -0,0 +1,11 @@
+public class InvokeExtension {
+    public static void main(String[] args) {
+        Raw t = new Raw();
+        t.test();
+        Raw_infoKt.info(t);
+        RawSub rs = new RawSub();
+        rs.test();
+        rs.sub();
+        Raw_infoKt.info(rs);
+    }
+}
