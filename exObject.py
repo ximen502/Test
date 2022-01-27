02eac4ff12ce8fcf5092330e7ba779ba9c5c62c6
ximen502 <ximen502@126.com>
2022-01-28 00:10:17 +0800

    包含object数据的list转换为json array的练习

--- exObject.py
+++ exObject.py
@@ -14,3 +14,21 @@ def obj2Dict(obj):
 usr = User("赵一伤", 40)
 uobj = json.dumps(usr, default=obj2Dict)
 print(uobj)
+
+usr2 = User("钱二败", 44)
+uobj2 = json.dumps(usr2, default=obj2Dict)
+print(uobj2)
+
+list = []
+list.append(usr)
+list.append(usr2)
+for u in list:
+    print(f"{u.name}, {u.age}")
+
+jsonArray = []
+jsonArray.append(uobj)
+jsonArray.append(uobj2)
+print(jsonArray)
+
+arr = json.dumps(jsonArray, ensure_ascii=False)
+print(arr)
