17d1dae0586b571ce0b6fce0c48a102db5c797a0
ximen502 <ximen502@126.com>
2022-02-19 08:22:56 +0800

    map作为属性委托的练习

---
+++ ExDelegateMap2.kt
@@ -0,0 +1,19 @@
+class MutableItem(val map: MutableMap<String, Any?>) {
+    var barCode: String by map
+    var name: String by map
+    var price: Double by map
+}
+
+fun main(args: Array<String>) {
+    val item = MutableItem(mutableMapOf())
+    item.barCode = "7654321"
+    item.name = "九阴真经"
+    item.price = 100.5
+
+    println("----------------")
+
+    val map = item.map
+    println(map["barCode"])
+    println(map["name"])
+    println(map["price"])
+}
