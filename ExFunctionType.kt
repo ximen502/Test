9af9ee4c4a2a1348101c215431f87d9264e1aea8
ximen502 <ximen502@126.com>
2022-03-05 00:06:16 +0800

    函数类型的练习

--- ExFunctionType.kt
+++ ExFunctionType.kt
@@ -7,6 +7,9 @@ func foo(a:Int, name:String) -> String{
 }
 该函数的类型是(Int, String)->String
 这种类型可以定义变量，修饰参数，修饰返回值类型
+
+当直接访问一个函数的引用，需要zai函数名前添加2个冒号，而且不能再后面添加圆括号，
+添加圆括号就变成了调用函数，而不是访问函数引用
  */
 fun pow(base: Int, exponent: Int): Int {
     var result = 1
@@ -16,7 +19,20 @@ fun pow(base: Int, exponent: Int): Int {
     return result
 }
 
+fun area(width: Int, height: Int): Int {
+    return width * height
+}
+
 fun main(args: Array<String>) {
     var myFun: (Int, Int) -> Int
+    var test: (String)
+
+    myFun = ::pow
+
+    println(myFun(2, 3))
+
+    myFun = ::area
+
+    println(myFun(4, 5))
 
 }
