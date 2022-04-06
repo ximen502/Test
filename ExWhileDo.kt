89ba2dee1539f3141455f5f503077e403b4ea201
ximen502 <ximen502@126.com>
2022-04-06 23:16:20 +0800

    kotlin while循环练习

---
+++ ExWhileDo.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    var c = 0
+    var sum = 0
+    while(c<100){
+        c++
+        sum += c
+        println(c)
+    }
+    println(sum)
+    println("loop end")
+}
